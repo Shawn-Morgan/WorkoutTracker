5b7881cad6a9e318b30bc2c0b523603090334b28
shawnm7474 <shawnm74@gmail.com>
2020-11-04 20:40:31 -0600

    fixed routing issue between controller and model workout...on to next issue

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+
+//++
+const logger = require("morgan");
+
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
 
+//++
+const db = require("./models");
+
 const app = express();
+
+//++
+const databaseName = "workout_db"
+
+//++
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,6 +40,8 @@ mongoose.connect(process.env.MONDODB_URI || 'mongodb+srv://shawnm74:RemakeMyself
 
 // routes
 app.use(require("./routes/api-routes.js"));
+//++
+app.use("/", require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
